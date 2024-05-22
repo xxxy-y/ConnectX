1e33eb4ab1ac78d0c5ae0d0b65f42e131efd6238
xxxy-y <wqt1922311386@outlook.com>
2024-05-22 19:53:58 +0800

    2024.5.22 19:53 项目骨架的搭建

---
+++ Connect-subject/ConnectX-subject-application/src/main/java/cn/edu/tyut/connectx/Main.java
@@ -0,0 +1,11 @@
+package cn.edu.tyut.connectx;
+
+/**
+ * @Author 吴庆涛
+ * @DATE 2024/5/22
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
